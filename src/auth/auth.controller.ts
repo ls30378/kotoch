1b0f786ec35c5a7eebe354415929cef6750ad37a
ls30378 <lorentsh@gmail.com>
2022-11-29 22:18:40 +0100

    Auth controllers done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { SignInDto } from './dto/signin.dto';
+import { AuthService } from './auth.service';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthDto, ForgotDto, ResetDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log('sign up');
+  }
+  @HttpCode(200)
+  @Post('signin')
+  signin(@Body() dto: SignInDto) {
+    console.log('sign in');
+  }
+
+  @Post('forgot')
+  forgotPassword(@Body() forgotDto: ForgotDto) {
+    console.log('forgot password');
+  }
+  @Post('reset')
+  resetPassword(@Body() resetDto: ResetDto) {
+    console.log('reset password');
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
+import { OptionalJwtStrategy } from './strategy/optional-jwt.strategy';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy, OptionalJwtStrategy],
 })
 export class AuthModule {}
