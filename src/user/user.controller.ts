d81765173a4413e06e25dae0ac282c545d5a0262
ls30378 <lorentsh@gmail.com>
2022-11-29 23:41:07 +0100

    Added user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { RolesGuard } from './../auth/guard/roles.guard';
+import { OptionalJwtGuard } from './../auth/guard/optional-jwt.guard';
+import { JwtGuard } from './../auth/guard/jwt.guard';
+import { UserService } from './user.service';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/decorator/roles.decorator';
+import { GetUser } from 'src/decorator/get-user.decorator';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('id/:id')
+  @UseGuards(OptionalJwtGuard, RolesGuard)
+  @Roles(0, 1, 2)
+  getUserDetails(@Param('id') id: string) {
+    console.log(id);
+  }
+
+  @Get('trending')
+  @UseGuards(OptionalJwtGuard, RolesGuard)
+  @Roles(0, 1, 2)
+  getTrendingUsers() {
+    console.log('trending');
+  }
+
+  @Post('like/:id')
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(1, 2)
+  likeUser(@GetUser('id') id: number, @Param('id') userId: number) {
+    console.log(id, userId);
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
